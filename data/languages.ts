4962db5fe1cbd605170d3d195fae53d102d652bc
Aiden Smith <29802327+DevVoxel@users.noreply.github.com>
2026-07-19 20:31:47 -0400

    reorder

--- data/languages.ts
+++ data/languages.ts
@@ -1,10 +1,10 @@
 // `bun run activity` prints numbers to paste here for easy updates.
 
 export const languages: { name: string; percent: number }[] = [
-  { name: 'TypeScript', percent: 72.3 },
-  { name: 'Go', percent: 28.1 },
-  { name: 'HTML', percent: 66.2 },
   { name: 'JavaScript', percent: 83.6 },
-  { name: 'GDScript', percent: 12.8 },
+  { name: 'TypeScript', percent: 72.3 },
+  { name: 'HTML', percent: 66.2 },
   { name: 'PLpgSQL', percent: 35.7 },
+  { name: 'Go', percent: 28.1 },
+  { name: 'GDScript', percent: 12.8 },
 ];
